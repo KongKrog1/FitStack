65f717a16380eef326b5e6fc12273456e265d9a5
OskarKrog <oskargislev@live.dk>
2025-10-07 14:09:36 +0200

    Add JwtBearer nuget package for auth

--- Backend/FitnessAppAPI/Program.cs
+++ Backend/FitnessAppAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using FitnessAppAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
